c212de72844a3e0b31d741c63af9c46aa4f79fed
mhoaglund <maxwell.hoaglund@gmail.com>
2017-06-02 14:13:11 -0500

    added path endpoint

---
+++ DivRodsUtilityAPI/DivRodsUtils/routes/path.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var router = express.Router();
+var async = require('async'), fs = require('fs');
+
+//GET device config
+router.get('/', function(req, res, next) {
+    
+});
+
+module.exports = router;
