09177ce45e206bdfa76f7b5b6d936730be997d07
Maxwell Hoaglund <maxwell.hoaglund@gmail.com>
2017-06-19 15:52:06 -0500

    added fake node for outside location and made location endpoint more robust

--- DivRodsUtilityAPI/DivRodsUtils/routes/locate.js
+++ DivRodsUtilityAPI/DivRodsUtils/routes/locate.js
@@ -4,22 +4,29 @@ var async = require('async'), fs = require('fs'), request = require('request');
 
 //Just passing a request through to the tracking API and responding with a small packet
 router.post('/', function(req, res, next) {
-    request.post(
-        FINDhost,
-        req.body,
-        function (error, response, body) {
-            if (!error && response.statusCode == 200) {
-                var jsonresp = JSON.parse(response.body);
-                //could do some massaging here
-                var devicelocation = jsonresp["location"];
-                console.log(jsonresp["location"])
-                if(req.query.deviceid){
-                    req.app.get('_DeviceSessions')._place(req.query.deviceid, devicelocation);
+    if(req.body && req.query.deviceid){
+        request.post(
+            FINDhost,
+            req.body,
+            function (error, response, body) {
+                if (!error && response.statusCode == 200) {
+                    var jsonresp = JSON.parse(response.body);
+                    //could do some massaging here
+                    var devicelocation = jsonresp["location"];
+                    if(req.query.deviceid){
+                        req.app.get('_DeviceSessions')._place(req.query.deviceid, devicelocation);
+                    }
+                    res.status(200).send(devicelocation);
                 }
-                res.status(200).send(devicelocation);
+                else{
+                    res.status(422).send("Error returned from FIND. Status code:" + response.statusCode + "Error:" + error);
+                }
             }
-        }
-    );
+        );
+    }
+    else{
+        res.status(404).send("Please enclose a valid device id and wifi fingerprint.");
+    }
 });
 
 module.exports = router;
