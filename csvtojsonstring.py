1c8ea01de6a16ade8bcb364bcca6ebcb5c5f42a7
mhoaglund <maxwell.hoaglund@gmail.com>
2017-09-27 13:10:23 -0500

    parsing json list from csv input for tagged pieces to save time

---
+++ csvtojsonstring.py
@@ -0,0 +1,31 @@
+import os
+import csv
+import json
+
+def intTryParse(value):
+    try:
+        return int(value), True
+    except ValueError:
+        return value, False
+
+entries = []
+
+with open('pieces.csv', 'rb') as csvfile:
+    piecereader = csv.reader(csvfile, delimiter=',', quotechar='|')
+    for row in piecereader:
+        idnumber = row[-1]
+        color = row[-2]
+        if not intTryParse(idnumber)[1]:
+            print 'Bad or empty ID'
+            continue
+        thisone = {
+            'artid': idnumber,
+            'color':  color
+        }
+        entries.append(thisone)
+        #print row
+
+
+with open('jsonpieces.txt', 'w') as outfile:  
+    print len(entries)
+    json.dump(entries, outfile)
