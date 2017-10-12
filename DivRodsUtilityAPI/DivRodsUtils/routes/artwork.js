0af77c89e40fb3928e6c5bc89b4ececb5499d133
mhoaglund <maxwell.hoaglund@gmail.com>
2017-10-11 21:53:18 -0500

    cleanup and lkgs backup

--- DivRodsUtilityAPI/DivRodsUtils/routes/artwork.js
+++ DivRodsUtilityAPI/DivRodsUtils/routes/artwork.js
@@ -4,26 +4,6 @@ var async = require('async'), fs = require('fs');
 var prefclient = require('../prefclient.js');
 var museum = require('../museum.js');
 
-//POST scanned artwork
-// router.get('/', function(req, res, next) {
-//     if(req.query.deviceid & req.query.artid & req.query.pref){
-//         //TODO lookup session_ID from device id
-//         //TODO send 0 or 1 from device and pass thru
-//         // var report = prefclient.record_preference(req.query.deviceid, req.query.artid, 1, function(data){
-//         //     if(data){
-//         //         console.log(data);
-//         //         res.send(data);
-//         //     }
-//         // });
-//         res.status(200).send("Got a scanned artwork...");
-//     }
-// });
-
-//adding a new work, more or less
-router.post('/register', function(req,res,next){
-
-});
-
 router.get('/all', function(req,res,next){
     var payload = _ArtFilter._overview();
     res.status(200).send(JSON.stringify(payload));
